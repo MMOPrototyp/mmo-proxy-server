215c645f5eb9570949641e650c28e170cdc621bf
JuKu <kuenzel.justin@t-online.de>
2020-05-27 23:58:59 +0200

    Initial commit

---
+++ mmo-proxy-server.cpp
@@ -0,0 +1,12 @@
+﻿// mmo-proxy-server.cpp: Definiert den Einstiegspunkt für die Anwendung.
+//
+
+#include "mmo-proxy-server.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
---
+++ mmo-proxy-server.h
@@ -0,0 +1,8 @@
+﻿// mmo-proxy-server.h: Includedatei für Include-Standardsystemdateien
+// oder projektspezifische Includedateien.
+
+#pragma once
+
+#include <iostream>
+
+// TODO: Verweisen Sie hier auf zusätzliche Header, die Ihr Programm erfordert.
