32f4d44b328ca51a972822c51b16f097c6050a81
JuKu <kuenzel.justin@t-online.de>
2020-05-28 00:46:20 +0200

    Change build.sh

--- build.sh
+++ build.sh
@@ -2,6 +2,8 @@
 
 # see also: https://stackoverflow.com/questions/1125968/how-do-i-force-git-pull-to-overwrite-local-files
 
+# see also: https://unix.stackexchange.com/questions/510744/conan-command-not-found
+
 mkdir build
 cd build
 conan install ..
