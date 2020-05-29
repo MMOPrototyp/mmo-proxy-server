bc7403e2151e8358e683e16b4c7e3319d6c73962
JuKu <kuenzel.justin@t-online.de>
2020-05-29 20:44:46 +0200

    Change travis ci configuration

---
+++ build_travis.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# see also: https://stackoverflow.com/questions/1125968/how-do-i-force-git-pull-to-overwrite-local-files
+
+# see also: https://unix.stackexchange.com/questions/510744/conan-command-not-found
+
+git submodule init && git submodule update
+
+cd 3rdparty/cpp_redis
+git submodule init && git submodule update
+cmake .
+cd ../../
+
+cd 3rdparty/config4cpp
+git submodule init && git submodule update
+make CDBG=-w
+cd ../../
+
+mkdir build
+cd build
+conan install ..
+
+cd ../
+
+make RedisCPP
