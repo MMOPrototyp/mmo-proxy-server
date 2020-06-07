8ab381d14c31356b948d20e08abd7e0b3b57fa50
JuKu <kuenzel.justin@t-online.de>
2020-06-07 20:11:15 +0200

    Add TCPMessage

---
+++ src/proxy/TCPMessage.h
@@ -0,0 +1,18 @@
+//
+// Created by Justin on 07.06.2020.
+//
+
+#ifndef PROXYSERVER_TCPMESSAGE_H
+#define PROXYSERVER_TCPMESSAGE_H
+
+
+namespace mmo {
+
+    class TCPMessage {
+
+    };
+
+}
+
+
+#endif //PROXYSERVER_TCPMESSAGE_H
--- src/proxy/TCPSession.h
+++ src/proxy/TCPSession.h
@@ -5,11 +5,93 @@
 #ifndef PROXYSERVER_TCPSESSION_H
 #define PROXYSERVER_TCPSESSION_H
 
+#include <cstdlib>
+#include <deque>
+#include <iostream>
+#include <list>
+#include <memory>
+#include <set>
+#include <utility>
+#include <boost/asio.hpp>
+
+using namespace std;
+using boost::asio::ip::tcp;
+
 
 namespace mmo {
 
-    class TCPSession {
+    class TCPSession : public std::enable_shared_from_this<TCPSession> {
+    public:
+        TCPSession(tcp::socket socket/*, chat_room& room*/)
+                : socket_(std::move(socket)),
+        //room_(room)
+        {
+        }
 
+        void start() {
+            //room_.join(shared_from_this());
+            do_read_header();
+        }
+
+        /*void deliver(const chat_message& msg)
+        {
+            bool write_in_progress = !write_msgs_.empty();
+            write_msgs_.push_back(msg);
+            if (!write_in_progress)
+            {
+                do_write();
+            }
+        }*/
+
+    private:
+        void do_read_header() {
+            auto self(shared_from_this());
+            boost::asio::async_read(socket_,
+                                    boost::asio::buffer(read_msg_.data(), chat_message::header_length),
+                                    [this, self](boost::system::error_code ec, std::size_t /*length*/) {
+                                        if (!ec && read_msg_.decode_header()) {
+                                            do_read_body();
+                                        } else {
+                                            room_.leave(shared_from_this());
+                                        }
+                                    });
+        }
+
+        void do_read_body() {
+            auto self(shared_from_this());
+            boost::asio::async_read(socket_,
+                                    boost::asio::buffer(read_msg_.body(), read_msg_.body_length()),
+                                    [this, self](boost::system::error_code ec, std::size_t /*length*/) {
+                                        if (!ec) {
+                                            room_.deliver(read_msg_);
+                                            do_read_header();
+                                        } else {
+                                            room_.leave(shared_from_this());
+                                        }
+                                    });
+        }
+
+        void do_write() {
+            auto self(shared_from_this());
+            boost::asio::async_write(socket_,
+                                     boost::asio::buffer(write_msgs_.front().data(),
+                                                         write_msgs_.front().length()),
+                                     [this, self](boost::system::error_code ec, std::size_t /*length*/) {
+                                         if (!ec) {
+                                             write_msgs_.pop_front();
+                                             if (!write_msgs_.empty()) {
+                                                 do_write();
+                                             }
+                                         } else {
+                                             room_.leave(shared_from_this());
+                                         }
+                                     });
+        }
+
+        tcp::socket socket_;
+        /*chat_room& room_;
+        chat_message read_msg_;
+        chat_message_queue write_msgs_;*/
     };
 
 }
