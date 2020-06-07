527e9687168190674b6116ac14a4e83f71491a42
JuKu <kuenzel.justin@t-online.de>
2020-06-07 19:45:49 +0200

    Add new TCPSession

---
+++ src/proxy/TCPSession.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Justin on 07.06.2020.
+//
+
+#include "TCPSession.h"
---
+++ src/proxy/TCPSession.h
@@ -0,0 +1,14 @@
+//
+// Created by Justin on 07.06.2020.
+//
+
+#ifndef PROXYSERVER_TCPSESSION_H
+#define PROXYSERVER_TCPSESSION_H
+
+
+class TCPSession {
+
+};
+
+
+#endif //PROXYSERVER_TCPSESSION_H
