65e32b5c61b160722d7faba73576b14495cbc2c2
JuKu <kuenzel.justin@t-online.de>
2020-06-07 19:30:50 +0200

    Add new TCPServer

---
+++ src/proxy/TCPServer.h
@@ -0,0 +1,18 @@
+//
+// Created by Justin on 07.06.2020.
+//
+
+#ifndef PROXYSERVER_TCPSERVER_H
+#define PROXYSERVER_TCPSERVER_H
+
+
+namespace mmo {
+
+    class TCPServer {
+
+    };
+
+}
+
+
+#endif //PROXYSERVER_TCPSERVER_H
